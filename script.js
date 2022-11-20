afc42e0a7a07abf0b335a2f9f39b518d85051f9d
Uttam <utam.shrestha65@gmail.com>
2022-11-20 20:03:26 +0545

    first screen done

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const screens = document.querySelectorAll(".screen")
+const choose_insect_btns = document.querySelectorAll(".choose-insect-btn")
+const start_btn = document.getElementById("start-btn")
+const game_container = document.getElementById("game-container")
+const timeEl = document.getElementById("time")
+const scoreEl = document.getElementById("score")
+const message = document.getElementById("message")
 
 
+let seconds = 0
+let score = 0
+let selected_insect = {}
+
+start_btn.addEventListener("click", ()=> screens[0].classList.add("up"))
